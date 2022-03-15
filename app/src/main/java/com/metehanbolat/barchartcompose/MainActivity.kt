3d729c602f060202034f8bd73d733c3dad6a77d3
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-03-15 23:16:53 +0300

    App completed.

--- app/src/main/java/com/metehanbolat/barchartcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/barchartcompose/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.metehanbolat.barchartcompose
 
+import android.graphics.Point
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.FloatTweenSpec
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.metehanbolat.barchartcompose.ui.theme.BarChartComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +29,104 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BarChartComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(
+                                        text = "Bar Chart",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            )
+                        }
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            BarChart()
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun identifyClickItem(
+    points: List<Point>,
+    x: Float,
+    y: Float
+): Int {
+    for ((index, point) in points.withIndex()) {
+        if (x > point.x + 20 && x < point.x + 20 + 40) {
+            return index
+        }
+    }
+    return -1
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    BarChartComposeTheme {
-        Greeting("Android")
+fun BarChart() {
+    val point = listOf(
+        Point(10, 10),
+        Point(90, 100),
+        Point(170, 30),
+        Point(250, 200),
+        Point(330, 120),
+        Point(410, 10),
+        Point(490, 200),
+        Point(570, 100),
+        Point(650, 10),
+        Point(730, 100),
+        Point(810, 200)
+    )
+
+    val context = LocalContext.current
+    var start by remember { mutableStateOf(false) }
+    val heightPre by animateFloatAsState(
+        targetValue = if (start) 1f else 0f,
+        animationSpec = FloatTweenSpec(duration = 1000)
+    )
+    
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .background(Color.White)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = {
+                        val i = identifyClickItem(point, it.x, it.y)
+                        Toast.makeText(context, "onTap: $i", Toast.LENGTH_SHORT).show()
+                    }
+                )
+            }
+    ) {
+        drawLine(
+            start = Offset(10f, 600f),
+            end = Offset(10f, 0f),
+            color = Color.Black,
+            strokeWidth = 3f
+        )
+        drawLine(
+            start = Offset(10f, 600f),
+            end = Offset(900f, 600f),
+            color = Color.Black,
+            strokeWidth = 3f
+        )
+        start = true
+
+        for (p in point) {
+            drawRect(
+                color = Color.Red,
+                topLeft = Offset(p.x + 30f, 600 - (600 - p.y) * heightPre),
+                size = Size(55f, (600 - p.y) * heightPre)
+            )
+        }
     }
 }
